fb30c6f09b13ef69d89491f20bad867720253368
Vanilla-Nya <151918371+Vanilla-Nya@users.noreply.github.com>
2025-02-04 08:03:39 +0700

    Create WSIB_Week1.java

---
+++ WSIB_Week1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class WSIB_Week1 {
+    public static int Answer;
+
+    public static void Qustion(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("100 - 99 = ");
+        Answer = sc.nextInt();
+        
+        if (Answer == 1) {
+            System.out.println("Right!");
+        } else {
+            System.out.println("Try Again!");
+            Qustion();
+        }
+    }
+    public static void main(String[] args) {
+        Qustion();
+    }
+}
