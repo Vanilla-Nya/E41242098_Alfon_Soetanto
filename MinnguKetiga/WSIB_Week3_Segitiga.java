28ce74016ba570676bd03904f5329454c486d1fc
Vanilla-Nya <151918371+Vanilla-Nya@users.noreply.github.com>
2025-02-20 12:39:52 +0700

    .

---
+++ MinnguKetiga/WSIB_Week3_Segitiga.java
@@ -0,0 +1,21 @@
+package MinnguKetiga;
+
+import java.util.Scanner;
+
+public class WSIB_Week3_Segitiga {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan Rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
